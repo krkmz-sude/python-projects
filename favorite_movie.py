f990b0333e2630aaf13bffc088403180dd39faaa
krkmz-sude <sudek5885@gmail.com>
2026-09-08 14:50:24 +0200

    Add files via upload

---
+++ favorite_movie.py
@@ -0,0 +1,4 @@
+movie1=input("What is your favorite movie?")
+movie2=input("What is your second favorite movie?")
+movie3=input("What is your third favorite movie?")
+movies = [movie1, movie2, movie3]
